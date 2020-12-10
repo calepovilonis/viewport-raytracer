b948cfec8c29f56230a0a92ef15572d46321131b
Cale Povilonis <calepovilonis@gmail.com>
2020-12-10 02:59:29 -0600

    Changes on 12/10/2020

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import pygame
 from pygame.locals import *
-
 from OpenGL.GL import *
 from OpenGL.GLU import *
-
+import matplotlib.pyplot as plt
 import math
 
 from shapes import *
+from matrixmath import *
+
+# ray tracing algorithm reference from https://medium.com/swlh/ray-tracing-from-scratch-in-python-41670e6a96f9
 
 pygame.init()
-display = (1280, 720)
+w = 852
+h = 480
+display = (w, h)
+
 screen = pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
 
 glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
@@ -24,6 +29,17 @@ glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE)
 glEnable(GL_LIGHT0)
 glLightfv(GL_LIGHT0, GL_AMBIENT, [0.5, 0.5, 0.5, 1])
 glLightfv(GL_LIGHT0, GL_DIFFUSE, [1.0, 1.0, 1.0, 1])
+light = np.array([7.,-5.,4.5])
+light_color = np.array([1.,1.,1.])
+light_ambient = np.array([1.,1.,1.])
+light_specular = np.array([1.,1.,1.])
+
+max_depth = 3
+col = np.zeros(3)
+image = np.zeros((h, w, 3))
+
+ratio = float(w) / h
+screen_project = (-1, 1 / ratio, 1, -1 / ratio)
 
 glMatrixMode(GL_PROJECTION)
 gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
@@ -32,6 +48,14 @@ glMatrixMode(GL_MODELVIEW)
 gluLookAt(0, -8, 0, 0, 0, 0, 0, 0, 1)
 viewMatrix = glGetFloatv(GL_MODELVIEW_MATRIX)
 glLoadIdentity()
+
+camera = np.array([0, 0, 0])
+camera_test = np.array([0., -6., 0.0])
+pixel_screen = [0.0, -5., 0.0]
+camera_rotation = [0, 0, 0]
+#camera_lookat = np.array([0.0,0.0,0.0])
+
+ambient_color = np.array([.05,.05,.05])
 
 # SHAPE INFO
 
@@ -47,10 +71,10 @@ vertices = (
     )
 
 plane_vertices = (
-    (-10,-10,-2),
-    (10,-10,-2),
-    (10,10,-2),
-    (-10,10,-2),
+    (-1000,-1000,-2),
+    (1000,-1000,-2),
+    (1000,1000,-2),
+    (-1000,1000,-2),
     )
 
 surfaces = (
@@ -62,7 +86,27 @@ surfaces = (
     (4,0,3,6)
     )
 
+gameobjects = []
 # END SHAPE INFO
+
+# begin ray tracing functions
+def normalize(v):
+    return v / np.linalg.norm(v)
+
+def reflected(v, axis):
+    return v - 2 * np.dot(v, axis) * axis
+
+def nearest_intersected_object(objects, ray_origin, ray_direction):
+    distances = [obj.intersect(obj.rtposition,ray_origin, ray_direction) for obj in objects]
+    nearest_object = None
+    min_distance = np.inf
+    for index, distance in enumerate(distances):
+        if distance and distance < min_distance:
+            min_distance = distance
+            nearest_object = objects[index]
+    return nearest_object, min_distance
+
+# end ray tracing functions
 
 # init mouse movement and center mouse on screen
 displayCenter = [screen.get_size()[i] // 2 for i in range(2)]
@@ -70,8 +114,15 @@ pygame.mouse.set_pos(displayCenter)
 mouseMove = [0, 0]
 
 up_down_angle = 0.0
+left_right_angle = 0.0
 paused = False
 run = True
+
+picked = 0
+placed = False
+rotate = False
+move = False
+
 while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -79,12 +130,45 @@ while run:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE or event.key == pygame.K_RETURN:
                 run = False
-            if event.key == pygame.K_PAUSE or event.key == pygame.K_p:
-                paused = not paused
-                pygame.mouse.set_pos(displayCenter)
-        # if not paused:
-        #     if event.type == pygame.MOUSEMOTION:
-        #         mouseMove = [event.pos[i] - mouseMove[i] for i in range(2)]
+            # if event.key == pygame.K_PAUSE or event.key == pygame.K_p:
+            #     paused = not paused
+            #     pygame.mouse.set_pos(displayCenter)
+            if event.key == pygame.K_PLUS:
+                print("plus")
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            # middle mouse button
+            if event.button == 2:
+                gameobjects[picked].isselected = False
+            # up scroll
+            if event.button == 4:
+                if (len(gameobjects) == 0):
+                    continue
+                if (len(gameobjects) == 1):
+                    picked = 0
+                    gameobjects[picked].isselected = True
+                else:
+                    prev = picked
+                    if picked == len(gameobjects)-1:
+                        picked = 0
+                    else:
+                        picked+=1
+                    gameobjects[picked].isselected = True
+                    gameobjects[prev].isselected = False
+            # down scroll
+            elif event.button == 5:
+                if (len(gameobjects) == 0):
+                    continue
+                if (len(gameobjects) == 1):
+                    picked = 0
+                    gameobjects[picked].isselected = True
+                else:
+                    prev = picked
+                    if picked == 0:
+                        picked = len(gameobjects)-1
+                    else:
+                        picked-=1
+                    gameobjects[picked].isselected = True
+                    gameobjects[prev].isselected = False
 
     if not paused:
         # get keys
@@ -100,6 +184,7 @@ while run:
         # apply the look up and down
             up_down_angle += mouseMove[1]*0.15
             glRotatef(up_down_angle, 1.0, 0.0, 0.0)
+            camera_rotation = [camera_rotation[0]+up_down_angle, camera_rotation[1], camera_rotation[2]]
         else:
             glRotatef(up_down_angle, 1.0, 0.0, 0.0)
 
@@ -108,24 +193,215 @@ while run:
         glLoadIdentity()
 
         if mousepress[0]:
+            left_right_angle = mouseMove[0]*0.15
             # apply the left and right rotation
-            glRotatef(mouseMove[0]*0.15, 0.0, 1.0, 0.0)
+            glRotatef(left_right_angle, 0.0, 1.0, 0.0)
+            camera_rotation = [camera_rotation[0], camera_rotation[1]+left_right_angle, camera_rotation[2]]
+
+        #print(camera)
 
         mods = pygame.key.get_mods()
+
+        selected = None
+        for obj in gameobjects:
+            if obj.isselected == True:
+                selected = obj
+                break
+
+
         # apply the movment
+        if keypress[pygame.K_r]:
+            print("Rendering to image...")
+            for i, y in enumerate(np.linspace(screen_project[1], screen_project[3], h)):
+                for j, x in enumerate(np.linspace(screen_project[0], screen_project[2], w)):
+                    pixel = np.array([x+pixel_screen[0], pixel_screen[1], y+pixel_screen[2]])
+                    origin = camera_test
+
+                    direction = normalize(pixel - origin)
+
+                    color = np.zeros((3))
+                    reflection = 1.
+
+                    for k in range(max_depth):
+                        # check for intersections
+                        nearest_object, min_distance = nearest_intersected_object(gameobjects, origin, direction)
+                        if nearest_object is None:
+                            continue
+
+                        # compute intersection point between ray and nearest object
+                        intersection = origin + min_distance * direction
+
+                        normal_to_surface = normalize(intersection - nearest_object.rtposition)
+                        shifted_point = intersection + 1e-5 * normal_to_surface
+                        intersection_to_light = normalize(light - shifted_point)
+
+                        _, min_distance = nearest_intersected_object(gameobjects, shifted_point, intersection_to_light)
+                        intersection_to_light_distance = np.linalg.norm(light - intersection)
+                        is_shadowed = min_distance < intersection_to_light_distance
+
+                        #print(min_distance, type(nearest_object), intersection, shifted_point, intersection_to_light_distance)
+
+                        if is_shadowed:
+                            continue
+
+                        # RGB
+                        illumination = np.zeros((3))
+
+                        # ambient
+                        illumination += nearest_object.ambient * light_ambient
+
+                        # diffuse
+                        illumination += nearest_object.color * light_color * np.dot(intersection_to_light, normal_to_surface)
+
+                        # specular
+                        intersection_to_camera = normalize(camera_test - intersection)
+                        H = normalize(intersection_to_light + intersection_to_camera)
+                        illumination += nearest_object.specular * light_specular * np.dot(normal_to_surface, H) ** (nearest_object.shininess / 4)
+
+                        # reflection
+                        color += reflection * illumination
+                        reflection *= nearest_object.reflection
+
+                        origin = shifted_point
+                        direction = reflected(direction, normal_to_surface)
+
+                    image[i, j] = np.clip(color, 0, 1)
+
+                print("progress: %d/%d" % (i + 1, h))
+            plt.imsave("render.png", image)
+            print("Image saved to directory.")
+
         if keypress[pygame.K_w]:
             glTranslatef(0,0,0.1)
+            camera[1] = camera[1]+.03
+            pixel_screen[1] = pixel_screen[1]+.03
         if keypress[pygame.K_s]:
             glTranslatef(0,0,-0.1)
+            camera[1] = camera[1]-.03
+            pixel_screen[1] = pixel_screen[1]-.03
         if keypress[pygame.K_d]:
             glTranslatef(-0.1,0,0)
+            camera[0] = camera[0]+.03
+            pixel_screen[0] = pixel_screen[0]+.03
         if keypress[pygame.K_a]:
             glTranslatef(0.1,0,0)
+            camera[0] = camera[0]-.03
+            pixel_screen[0] = pixel_screen[0]-.03
         if keypress[pygame.K_SPACE]:
             if mods & pygame.KMOD_SHIFT:
                 glTranslatef(0, 0.1, 0)
+                camera[2] = camera[2]+.03
+                pixel_screen[2] = pixel_screen[2]+.03
             else:
                 glTranslatef(0, -0.1, 0)
+                camera[2] = camera[2]-.03
+                pixel_screen[2] = pixel_screen[2]-.03
+
+        vm = glGetFloatv(GL_MODELVIEW_MATRIX)
+        invert = InverseMat44(vm)
+        camera = [camera[0]+invert[12], camera[1]+invert[13], camera[2]+invert[14]]
+
+        if keypress[pygame.K_l]:
+            try:
+                lx = float(input("Light x position: "))
+                ly = float(input("Light y position: "))
+                lz = float(input("Light z position: "))
+
+                light[0] = lx
+                light[1] = ly
+                light[2] = lz
+            except:
+                continue
+        if keypress[pygame.K_p]:
+            if placed == False:
+                plane = Plane(plane_vertices, [.5,.5,.5], 0.00, 0)
+                plane.position = np.array([0.0, 0.0, 0.0])
+                for obj in gameobjects:
+                    if obj.isselected == True:
+                        obj.isselected = False
+                plane.isselected = True
+                gameobjects.append(plane)
+                picked = len(gameobjects)-1
+                placed = True
+        elif keypress[pygame.K_c]:
+            if placed == False:
+                cube = Cube(vertices, surfaces, [.5,.5,.5], 0.25, 75)
+                cube.position = np.array([0.,0.,0.])
+                for obj in gameobjects:
+                    if obj.isselected == True:
+                        obj.isselected = False
+                cube.isselected = True
+                picked = len(gameobjects)-1
+                gameobjects.append(cube)
+                placed = True
+        elif keypress[pygame.K_o]:
+            if placed == False:
+                sphere = Sphere(1.0, 128, 128, [.5,.5,.5], 0.5, 100)
+                sphere.position = np.array([0.0, 0, 0.0])
+                for obj in gameobjects:
+                    if obj.isselected == True:
+                        obj.isselected = False
+                sphere.isselected = True
+                picked = len(gameobjects)-1
+                gameobjects.append(sphere)
+                placed = True
+        else:
+            placed = False
+        if selected is not None:
+            if keypress[pygame.K_n]:
+                r = input("Material Red value (0-1): ")
+                g = input("Material Green value (0-1): ")
+                b = input("Material Blue value (0-1): ")
+                reflect = input("Material Reflection value (0-1): ")
+                s = input("Material Shininess value (0-100): ")
+
+                selected.color[0] = float(r)
+                selected.color[1] = float(g)
+                selected.color[2] = float(b)
+                selected.reflection = float(reflect)
+                selected.shininess = float(s)
+
+            if keypress[pygame.K_m]:
+                move = True
+                rotate = False
+            if keypress[pygame.K_COMMA]:
+                move = False
+                rotate = True
+            if keypress[pygame.K_BACKSPACE]:
+                selected.isselected = False
+                if picked == len(gameobjects)-1:
+                    picked-=1
+                gameobjects.remove(selected)
+            if keypress[pygame.K_UP]:
+                if move:
+                    if mods & pygame.KMOD_SHIFT:
+                        selected.move(0, 0, .1)
+                    else:
+                        selected.move(0, .1, 0)
+                if rotate:
+                    selected.rotate_x(-.5)
+            if keypress[pygame.K_DOWN]:
+                if move:
+                    if mods & pygame.KMOD_SHIFT:
+                        selected.move(0, 0, -.1)
+                    else:
+                        selected.move(0, -.1, 0)
+                if rotate:
+                        selected.rotate_x(.5)
+            if keypress[pygame.K_LEFT]:
+                if move:
+                    selected.move(-.1, 0, 0)
+                if rotate:
+                    selected.rotate_y(-.5)
+            if keypress[pygame.K_RIGHT]:
+                if move:
+                    selected.move(.1, 0, 0)
+                if rotate:
+                    selected.rotate_y(.5)
+            if keypress[pygame.K_KP_PLUS] or keypress[pygame.K_RIGHTBRACKET]:
+                selected.scale(.05)
+            if keypress[pygame.K_KP_MINUS] or keypress[pygame.K_LEFTBRACKET]:
+                selected.scale(-.05)
 
         # multiply the current matrix by the get the new view matrix and store the final vie matrix
         glMultMatrixf(viewMatrix)
@@ -141,19 +417,8 @@ while run:
 
         glPushMatrix()
 
-        plane1 = Plane(plane_vertices)
-        plane1.draw_plane()
-
-        cube1 = Cube(vertices, surfaces)
-        cube1.draw_cube()
-
-        sphere1 = Sphere(1.0, 128, 128)
-        glTranslatef(-3, 0, 0)
-        sphere1.draw_sphere()
-
-        sphere2 = Sphere(1.0, 128, 128)
-        glTranslatef(6, 0, 0)
-        sphere2.draw_sphere()
+        for obj in gameobjects:
+            obj.draw()
 
         glPopMatrix()
 
--- shapes.py
+++ shapes.py
@@ -1,32 +1,98 @@
 import pygame
 from pygame.locals import *
-
 from OpenGL.GL import *
 from OpenGL.GLU import *
-
 import math
+import numpy as np
 
 class Plane:
-    def __init__(plane, vertices):
+    def __init__(plane, vertices, color, reflection, shininess):
         plane.vertices = vertices
+        plane.isselected = False
+        plane.position = np.array([0,0,0])
+        plane.rotation_x = 0
+        plane.rotation_y = 0
+        plane.color = color
+        plane.reflection = reflection
+        plane.normal = np.array([0.0,0.0,1.0])
+        plane.ambient = np.array([plane.color[0]/5,plane.color[1]/5,plane.color[2]/5])
+        plane.specular = np.array([1.,1.,1.])
+        plane.shininess = shininess
+        plane.rtposition = [plane.position[0], plane.position[1], plane.position[2]-2.0]
 
-    def draw_plane(plane):
+    def draw(plane):
+        glTranslatef(plane.position[0], plane.position[1], plane.position[2])
+        #glPushMatrix()
+        glRotated(plane.rotation_x,1,0,0)
+        glRotated(plane.rotation_y,0,1,0)
         glBegin(GL_QUADS)
         for vertex in plane.vertices:
-            glColor4f(0.5, 0.5, 0.5, 1)
+            if plane.isselected:
+                glColor4f(1, 1, 1, 1)
+            else:
+                glColor4f(plane.color[0], plane.color[1], plane.color[0], 1)
             glVertex3fv(vertex)
         glEnd()
+        #glPopMatrix()
+        glTranslatef(-plane.position[0], -plane.position[1], -plane.position[2])
+
+
+    def move(plane, x, y, z):
+        plane.position = np.array([plane.position[0] + x, plane.position[1] + y, plane.position[2] + z])
+        plane.rtposition =  [plane.position[0], plane.position[1], plane.position[2]-2.0]
+
+    def scale(plane, value):
+        plane.vertices = (
+            (plane.vertices[0][0]-value, plane.vertices[0][1]-value, plane.vertices[0][2]),
+            (plane.vertices[1][0]+value, plane.vertices[1][1]-value, plane.vertices[1][2]),
+            (plane.vertices[2][0]+value, plane.vertices[2][1]+value, plane.vertices[2][2]),
+            (plane.vertices[3][0]-value, plane.vertices[3][1]+value, plane.vertices[3][2]),
+            )
+
+    def rotate_x(plane, a):
+        plane.rotation_x += a
+
+    def rotate_y(plane, a):
+        plane.rotation_y += a
+
+    def intersect(plane, P, O, D):
+        N = plane.normal
+        denom = np.dot(N,D)
+        if np.abs(denom) < 0.0001:
+            return None
+        d_new = np.dot(P-O,N) / denom
+        if d_new < 0.0001:
+            return None
+        return d_new
 
 class Cube:
-    def __init__(cube, vertices, surfaces):
+    def __init__(cube, vertices, surfaces, color, reflection, shininess):
         cube.vertices = vertices
         cube.surfaces = surfaces
+        cube.isselected = False
+        cube.position = np.array([0,0,0])
+        cube.rotation_x = 0
+        cube.rotation_y = 0
+        cube.color = color
+        cube.reflection = reflection
+        cube.normal = np.array([0.0,0.0,1.0])
+        cube.ambient = np.array([cube.color[0]/5,cube.color[1]/5,cube.color[2]/5])
+        cube.specular = np.array([1.,1.,1.])
+        cube.shininess = shininess
+        cube.rtposition = [cube.position[0], cube.position[1], cube.position[2]]
+        cube.scaleof = 1.
 
-    def draw_cube(cube):
+    def draw(cube):
+        glTranslatef(cube.position[0], cube.position[1], cube.position[2])
+        glPushMatrix()
+        glRotated(cube.rotation_x,1,0,0)
+        glRotated(cube.rotation_y,0,1,0)
         glBegin(GL_QUADS)
         for surface in cube.surfaces:
             for vertex in surface:
-                color = (0, .5, 0)
+                color = cube.color
+                if cube.isselected:
+                    color = (1.,1.,1.)
                 glColor3fv(color)
                 glVertex3f(
                 cube.vertices[vertex][0],
@@ -34,14 +100,136 @@ class Cube:
                 cube.vertices[vertex][2]
                 )
         glEnd()
+        glPopMatrix()
+        glTranslatef(-cube.position[0], -cube.position[1], -cube.position[2])
+
+    def move(cube, x, y, z):
+        cube.position = [cube.position[0] + x, cube.position[1] + y, cube.position[2] + z]
+        cube.rtposition = cube.position
+        # print(cube.rtposition)
+
+    def scale(cube, value):
+        cube.vertices = (
+            (cube.vertices[0][0]+value, cube.vertices[0][1]-value, cube.vertices[0][2]-value),
+            (cube.vertices[1][0]+value, cube.vertices[1][1]+value, cube.vertices[1][2]-value),
+            (cube.vertices[2][0]-value, cube.vertices[2][1]+value, cube.vertices[2][2]-value),
+            (cube.vertices[3][0]-value, cube.vertices[3][1]-value, cube.vertices[3][2]-value),
+            (cube.vertices[4][0]+value, cube.vertices[4][1]-value, cube.vertices[4][2]+value),
+            (cube.vertices[5][0]+value, cube.vertices[5][1]+value, cube.vertices[5][2]+value),
+            (cube.vertices[6][0]-value, cube.vertices[6][1]-value, cube.vertices[6][2]+value),
+            (cube.vertices[7][0]-value, cube.vertices[7][1]+value, cube.vertices[7][2]+value)
+            )
+
+        cube.scaleof += value
+
+    def rotate_x(cube, a):
+        cube.rotation_x += a
+
+    def rotate_y(cube, a):
+        cube.rotation_y += a
+
+    def intersect(cube, P, O, D):
+        min = [cube.rtposition[0]+cube.scaleof, cube.rtposition[1]-cube.scaleof, cube.rtposition[2]-cube.scaleof]
+        max = [cube.rtposition[0]-cube.scaleof, cube.rtposition[1]+cube.scaleof, cube.rtposition[2]+cube.scaleof]
+
+        tmin = (min[0] - O[0]) / D[0]
+        tmax = (max[0] - O[0]) / D[0]
+
+        if (tmin > tmax):
+            tmp = tmin
+            tmin = tmax
+            tmax = tmp
+
+        tymin = (min[1] - O[1]) / D[1]
+        tymax = (max[1] - O[1]) / D[1]
+
+        if (tymin > tymax):
+            tmp = tymin
+            tymin = tymax
+            tymax = tmp
+
+        if ((tmin > tymax) or (tymin > tmax)):
+            return None
+
+        if (tymin > tmin):
+            tmin = tymin
+        if (tymax < tmax):
+            tmax = tymax
+
+        tzmin = (min[2] - O[2]) / D[2]
+        tzmax = (max[2] - O[2]) / D[2]
+
+        if (tzmin > tzmax):
+            tmp = tzmin
+            tzmin = tzmax
+            tzmax = tmp
+
+        if ((tmin > tzmax) or (tzmin > tmax)):
+            return None
+
+        if (tzmin > tmin):
+            tmin = tzmin
+        if (tzmax < tmax):
+            tmax = tzmax
+
+        if (.01 <= tmin < tmax):
+            return tmin
+        elif (0.01 <= tymin < tmin and 0.01 <= tymin < tzmin):
+            return tymin
+        elif (0.01 <= tzmin < tmin and 0.01 <= tzmin < tymin):
+            return tzmin
+        else:
+            return None
+
 
 class Sphere:
-    def __init__(sphere, radius, slices, stacks):
-        sphere.radius = radius
+    def __init__(sphere, radius, slices, stacks, color, reflection, shininess):
+        sphere.radius = np.array(radius)
         sphere.slices = slices
         sphere.stacks = stacks
+        sphere.isselected = False
+        sphere.position = np.array([0,0,0])
+        sphere.rotation_x = 0
+        sphere.rotation_y = 0
+        sphere.color = color
+        sphere.reflection = reflection
+        sphere.ambient = np.array([sphere.color[0]/5,sphere.color[1]/5,sphere.color[2]/5])
+        sphere.specular = np.array([1.,1.,1.])
+        sphere.shininess = shininess
+        sphere.rtposition = sphere.position
 
-    def draw_sphere(sphere):
-        glColor4f(0.5, 0.2, 0.2, .5)
+    def draw(sphere):
+        glTranslatef(sphere.position[0], sphere.position[1], sphere.position[2])
+        if sphere.isselected:
+            glColor4f(1, 1, 1, 1)
+        else:
+            glColor4f(sphere.color[0], sphere.color[1], sphere.color[2], 1)
         newSphere = gluNewQuadric()
         gluSphere(newSphere, sphere.radius, sphere.slices, sphere.stacks)
+        glTranslatef(-sphere.position[0], -sphere.position[1], -sphere.position[2])
+
+    def move(sphere, x, y, z):
+        sphere.position = np.array([sphere.position[0] + x, sphere.position[1] + y, sphere.position[2] + z])
+        sphere.rtposition = sphere.position
+        #print(sphere.position)
+
+    def scale(sphere, value):
+        sphere.radius += value
+
+    def rotate_x(sphere, a):
+        sphere.rotation_x += a
+
+    def rotate_y(sphere, a):
+        sphere.rotation_y += a
+
+    def intersect(sphere, center, ray_origin, ray_direction):
+        radius = sphere.radius
+        b = 2 * np.dot(ray_direction, ray_origin - center)
+        c = np.linalg.norm(ray_origin - center) ** 2 - radius ** 2
+        delta = b ** 2 - 4 * c
+        if delta > 0:
+            t1 = (-b + np.sqrt(delta)) / 2
+            t2 = (-b - np.sqrt(delta)) / 2
+            if t1 > 0 and t2 > 0:
+                return min(t1, t2)
+        return None
